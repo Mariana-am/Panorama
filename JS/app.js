e9a75d2714e53d63793d4123c70038d6e5527d3a
Mariana <mariana.antunes.marcal@gmail.com>
2018-10-25 21:38:40 +0100

    added boostrap and Font Awesome

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,21 @@
 //alert('hello world')
+
+function iswindy(speed) {
+    if (speed > 5){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (iswindy(3) === true) {
+    alert('it is windy');
+} else {
+    alert('it is not windy');
+}
+
+if (iswindy(10) === true) {
+    alert('it is windy');
+} else {
+    alert('it is not windy');
+}
